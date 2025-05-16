1f32e2e7c71230e11e2f5c8457e62e0877e99dde
funksam006 <funksam006@gmail.com>
2025-05-16 11:15:19 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,22 @@
-transformIgnorePatterns: [
-  'node_modules/(?!(react-native' +
-    '|@react-native' +
-    '|react-redux' +
-    '|@react-navigation' +
-    '|redux-persist' +
-    '|react-native-gesture-handler' +
-    '|react-native-toast-message' +
-    '|react-native-splash-screen' +
-    '|react-native-status-bar-height' +
-    '|react-native-modal-datetime-picker' +
-    '|@react-native-community/datetimepicker' + // ✅ Add this
-    ')/)',
-],
+module.exports = {
+  preset: 'react-native',
+  setupFiles: ['./jest/setup.js'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|react-redux' +
+      '|@react-navigation' +
+      '|redux-persist' +
+      '|react-native-gesture-handler' +
+      '|react-native-toast-message' +
+      '|react-native-splash-screen' +
+      '|react-native-status-bar-height' +
+      '|react-native-modal-datetime-picker' +
+      '|@react-native-community/datetimepicker' +
+      ')/)',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp
