91dbd12f02dfd03f5b035aed6d0b2a2354a810d5
funksam006 <funksam006@gmail.com>
2025-05-16 13:58:41 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -5,17 +5,18 @@ module.exports = {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native' +
-      '|@react-native' +
-      '|@react-navigation' +
-      '|react-native-gesture-handler' +
-      '|react-native-toast-message' +
-      '|react-native-splash-screen' +
-      '|react-native-status-bar-height' +
-      '|react-native-modal-datetime-picker' +
-      '|react-native-country-codes-picker' +
-      ')/)',
-  ],
+  'node_modules/(?!(react-native' +
+    '|@react-native' +
+    '|@react-navigation' +
+    '|@react-native-community/datetimepicker' +
+    '|react-native-gesture-handler' +
+    '|react-native-toast-message' +
+    '|react-native-splash-screen' +
+    '|react-native-status-bar-height' +
+    '|react-native-modal-datetime-picker' +
+    '|react-native-country-codes-picker' +
+    ')/)'
+],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/jest/__mocks__/fileMock.js',
     '\\.(css|less)$': '<rootDir>/jest/__mocks__/styleMock.js',
