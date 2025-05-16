6fcb748aaad4244ff25161cb063242b8196e4853
funksam006 <funksam006@gmail.com>
2025-05-16 10:18:07 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['./jest/setup.js'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|react-redux|@react-navigation|redux-persist|react-native-gesture-handler)/)'
-  ],
   transform: {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native' +
+      '|@react-native' +
+      '|react-redux' +
+      '|@react-navigation' +
+      '|redux-persist' +
+      '|react-native-gesture-handler' +
+      '|react-native-toast-message' +
+      ')/)',
+  ],
 };
