0286e66a3908695dd57580c4322337e1e5102fa1
funksam006 <funksam006@gmail.com>
2025-05-16 11:14:41 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,14 @@
-module.exports = {
-  preset: 'react-native',
-  setupFiles: ['./jest/setup.js'],
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native' +
-      '|@react-native' +
-      '|react-redux' +
-      '|@react-navigation' +
-      '|redux-persist' +
-      '|react-native-gesture-handler' +
-      '|react-native-toast-message' +
-      '|react-native-splash-screen' +
-      '|react-native-status-bar-height' +
-      '|react-native-modal-datetime-picker' +  
-      ')/)',
-  ],
-  moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/jest/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/jest/__mocks__/styleMock.js',
-  },
-  testEnvironment: 'jest-environment-jsdom',
-};
+transformIgnorePatterns: [
+  'node_modules/(?!(react-native' +
+    '|@react-native' +
+    '|react-redux' +
+    '|@react-navigation' +
+    '|redux-persist' +
+    '|react-native-gesture-handler' +
+    '|react-native-toast-message' +
+    '|react-native-splash-screen' +
+    '|react-native-status-bar-height' +
+    '|react-native-modal-datetime-picker' +
+    '|@react-native-community/datetimepicker' + // ✅ Add this
+    ')/)',
+],
